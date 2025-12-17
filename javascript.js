017a4e7cbcc9dfcc7dba2cd4d0f9000536f7eaa7
Avery McLean <mcleanavery04@gmail.com>
2025-12-17 15:33:54 -0500

    Completed revisiting Rock Paper Scissors.

--- javascript.js
+++ javascript.js
@@ -9,64 +9,147 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() {
-    let humanChoice = prompt("What do you choose?");
-    humanChoice = humanChoice.toLocaleLowerCase();
-    return humanChoice;
+// function getHumanChoice() {
+//     // let humanChoice = prompt("What do you choose?");
+//     humanChoice = humanChoice.toLocaleLowerCase();
+//     return humanChoice;
 
-}
+// }
 
+let humanScore = 0;
+let computerScore = 0;
 
-
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
-
-    function playRound(humanChoice, computerChoice) {
+function playRound(humanChoice, computerChoice) {
+    let result = ""
         if (humanChoice == 'rock') {
             if (computerChoice == 'paper') {
-                console.log("You lose! Paper beats rock");
+                result = "You lose! Paper beats rock";
                 computerScore += 1;
             } else if (computerChoice == 'scissors') {
-                console.log("You win! Rock beats scissors");
+                result = "You win! Rock beats scissors";
                 humanScore += 1;
             } else {
-                console.log("Tie!");
+                result = "Tie!";
             }
         } else if (humanChoice == 'paper') {
             if (computerChoice == 'scissors') {
-                console.log ("You lose! Scissors beats paper");
+                result = "You lose! Scissors beats paper";
                 computerScore += 1;
             } else if (computerChoice == 'rock') {
-                console.log("You win! Paper beats rock");
+                result = "You win! Paper beats rock";
                 humanScore += 1;
             } else {
-                console.log("Tie!");
+                result = "Tie!";
             }
         } else if (humanChoice == 'scissors') {
             if (computerChoice == 'rock') {
-                console.log("You lose! Rock beats scissors");
+                result = "You lose! Rock beats scissors";
                 computerScore += 1;
             } else if (computerChoice == 'paper') {
-                console.log("You win! Scissors beats paper");
+                result = "You win! Scissors beats paper";
                 humanScore += 1;
             } else {
-                console.log("Tie!");
+                result = "Tie!";
             }
         }
+        return result;
     }
 
-    for (i = 0; i < 5; i++) {
-        playRound(getHumanChoice(), getComputerChoice());
-    }
-    if (humanScore > computerScore) {
-        console.log("Congrats! You win!");
-    } else if (computerScore > humanScore) {
-        console.log("Too bad so sad - you lose!");
-    } else {
-        console.log("It's a tie!");
-    }
 
-}
+// playRound(getHumanChoice(), getComputerChoice());
 
-playGame();
+// function playGame() {
+//     let humanScore = 0;
+//     let computerScore = 0;
+
+//     function playRound(humanChoice, computerChoice) {
+//         if (humanChoice == 'rock') {
+//             if (computerChoice == 'paper') {
+//                 console.log("You lose! Paper beats rock");
+//                 computerScore += 1;
+//             } else if (computerChoice == 'scissors') {
+//                 console.log("You win! Rock beats scissors");
+//                 humanScore += 1;
+//             } else {
+//                 console.log("Tie!");
+//             }
+//         } else if (humanChoice == 'paper') {
+//             if (computerChoice == 'scissors') {
+//                 console.log ("You lose! Scissors beats paper");
+//                 computerScore += 1;
+//             } else if (computerChoice == 'rock') {
+//                 console.log("You win! Paper beats rock");
+//                 humanScore += 1;
+//             } else {
+//                 console.log("Tie!");
+//             }
+//         } else if (humanChoice == 'scissors') {
+//             if (computerChoice == 'rock') {
+//                 console.log("You lose! Rock beats scissors");
+//                 computerScore += 1;
+//             } else if (computerChoice == 'paper') {
+//                 console.log("You win! Scissors beats paper");
+//                 humanScore += 1;
+//             } else {
+//                 console.log("Tie!");
+//             }
+//         }
+//     }
+
+//     for (i = 0; i < 5; i++) {
+//         playRound(getHumanChoice(), getComputerChoice());
+//     }
+//     if (humanScore > computerScore) {
+//         console.log("Congrats! You win!");
+//     } else if (computerScore > humanScore) {
+//         console.log("Too bad so sad - you lose!");
+//     } else {
+//         console.log("It's a tie!");
+//     }
+
+// }
+
+// playGame();
+
+const buttons = document.querySelectorAll("button");
+
+const resultsDiv = document.querySelector("#results");
+
+const playerScoreDisplay = document.querySelector(".playerScore");
+const computerScoreDisplay = document.querySelector(".computerScore");
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        const playerChoice = button.textContent.toLowerCase();
+
+        if (humanScore < 5 && computerScore < 5) {
+            const roundResult = playRound(playerChoice, getComputerChoice());
+            resultsDiv.textContent = roundResult;
+
+            playerScoreDisplay.textContent = `Your Score: ${humanScore}`;
+            computerScoreDisplay.textContent = `Computer Score: ${computerScore}`;
+        }
+        
+
+        
+
+        if (humanScore == 5) {
+            const victoryMessage = document.createElement("div");
+            victoryMessage.textContent = "You WIN!!!!";
+            victoryMessage.style.color = "green";
+            
+            const board = document.querySelector(".board");
+
+            board.append(victoryMessage);
+        }
+
+        if (computerScore == 5) {
+            const lossMessage = document.createElement("div");
+            lossMessage.textContent = "You lose :(";
+            lossMessage.style.color = "red";
+
+            const board = document.querySelector(".board");
+            board.append(lossMessage);
+        }
+    });
+});
